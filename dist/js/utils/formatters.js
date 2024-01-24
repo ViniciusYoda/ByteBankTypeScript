49a1c96e8bef1a5f2a0a77bfb860fb9ddcb124dd
Vinícius Yoda <vyoda4604@gmail.com>
2024-01-24 20:31:26 -0300

    ts-js

--- dist/js/utils/formatters.js
+++ dist/js/utils/formatters.js
@@ -1,10 +1,8 @@
 import { FormatoData } from "../types/FormatoData.js";
-
-export function formatarMoeda(valor: number): string {
+export function formatarMoeda(valor) {
     return valor.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
 }
-
-export function formatarData(data: Date, formato: FormatoData = FormatoData.PADRAO): string {
+export function formatarData(data, formato = FormatoData.PADRAO) {
     if (formato === FormatoData.DIA_SEMANA_DIA_MES_ANO) {
         return data.toLocaleDateString("pt-br", {
             weekday: "long",
@@ -16,6 +14,5 @@ export function formatarData(data: Date, formato: FormatoData = FormatoData.PADR
     else if (formato === FormatoData.DIA_MES) {
         return data.toLocaleDateString("pt-br", { day: "2-digit", month: "2-digit" });
     }
-
     return data.toLocaleDateString("pt-br");
 }
